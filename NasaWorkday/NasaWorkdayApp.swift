c64909dfa253cd0be12b21368ac2d06e22a42aaa
fernando babonoyaba <babonoyaba89@gmail.com>
2023-05-09 16:18:15 -0600

    Initial Commit

---
+++ NasaWorkday/NasaWorkdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaWorkdayApp.swift
+//  NasaWorkday
+//
+//  Created by fernando babonoyaba on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NasaWorkdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
